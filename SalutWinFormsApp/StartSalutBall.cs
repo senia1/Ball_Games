72eab941345951d04589465237542507b4568145
oksit <oks314@gmail.com>
2023-04-14 14:07:51 +0300

    -

--- SalutWinFormsApp/StartSalutBall.cs
+++ SalutWinFormsApp/StartSalutBall.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace SalutWinFormsApp
 {
-    internal class StartSalutBall
+    public class StartSalutBall : SalutBall
     {
+        public event EventHandler<HitEventArgs> TopReached;
+        public StartSalutBall(Form form) : base(form, form.ClientSize.Width / 2, form.ClientSize.Height)
+        {
+            vy = (float)random.NextDouble() * -6 - 7;
+            vx = 0;
+        }
+
+        protected override void Go()
+        {
+            base.Go();
+
+            if (vy > 0)
+            {
+                Stop();
+                Clear();
+                TopReached?.Invoke(this, new HitEventArgs(centerX, centerY));
+            }
+        }
     }
 }
