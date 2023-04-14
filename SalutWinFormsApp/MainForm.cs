5c45c00af91f231d7ccb2040ad8d0a95e45a382a
oksit <oks314@gmail.com>
2023-04-14 14:04:31 +0300

    added class SalutBall

--- SalutWinFormsApp/MainForm.cs
+++ SalutWinFormsApp/MainForm.cs
@@ -1,10 +1,29 @@
+using System;
+
 namespace SalutWinFormsApp
 {
     public partial class MainForm : Form
     {
+        Random random = new Random();
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartSalutBall startSalutball = new StartSalutBall(this);
+            startSalutball.TopReached += Ball_TopReached;
+            startSalutball.Start();
+        }
+
+        private void MainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            for (int i = 0; i < random.Next(4, 10); i++)
+            {
+                var ball = new SalutBall(this, e.X, e.Y);
+                ball.Start();
+            }
         }
     }
 }
---
+++ SalutWinFormsApp/SalutBall.cs
@@ -0,0 +1,22 @@
+﻿using Balls.Common;
+
+namespace SalutWinFormsApp
+{
+    public class SalutBall : MoveBall
+    {
+        private float g = 0.2f;
+        public SalutBall(Form form, float centerX, float centerY) : base(form)
+        {
+            radius = 15;
+            this.centerX = centerX;
+            this.centerY = centerY;
+            vy = -Math.Abs(vy);
+        }
+
+        protected override void Go()
+        {
+            base.Go();
+            vy += g;
+        }
+    }
+}
